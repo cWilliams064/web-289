d8458183c34c4b409e12bba1303d2c2dc2ec26e4
Courtney Williams <courtneyrwilliams@students.abtech.edu>
2025-02-22 22:20:19 -0500

    completed key portions and css.

--- public/index.php
+++ public/index.php
@@ -15,10 +15,8 @@
           <li><a href="index.php">Home</a></li>
           <li><a href="#">Recipes</a></li>
           <li><a href="about.html">About Us</a></li>
-          <section>
-            <li><a href="#" id="sidebar-opener">Log In</a></li>
-            <li><a href="#"><img src="assets/login-image.png" width="27" height="27" alt="User icon that links to login."></a></li>
-          </section>
+          <li><a href="#" id="sidebar-opener">Log In</a></li>
+          <li><a href="#"><img src="assets/login-image.png" width="27" height="27" alt="User icon that links to login."></a></li>
         </ul>
       </nav>
       <section>
@@ -54,28 +52,29 @@
           </section>
           <img src="assets/h1-image.png" width="1084" height="427" alt="Picnic foods with a juice pitcher on top of a wooden counter in a kitchen with the sun shining in.">
         </section>
-        <h2>Popular Categories</h2>
-        <section>
+        <section id="popular-categories">
+          <h2>Popular Categories</h2>
           <section>
-            <h3>Breakfast</h3>
+            <a href="#"><h3>Breakfast</h3></a>
             <img src="assets/home-category/breakfast-category.png" width="322" height="395" alt="An egg croissant layered breakfast meal.">
           </section>
           <section>
-            <h3>Vegan</h3>
+            <a href="#"><h3>Vegan</h3></a>
             <img src="assets/home-category/vegan-category.png" width="322" height="395" alt="Vegan meal with avocado slices and egg on whole wheat bread.">
           </section>
           <section>
-            <h3>Italian</h3>
+            <a href="#"><h3>Italian</h3></a>
             <img src="assets/home-category/italian-category.png" width="322" height="395" alt="Italian pasta with spaghetti noodles with tomatoes and herbs.">
           </section>
         </section>
-
-        <article>
-          <h2>The Heart of Grandma's Pantry</h2>
-          <p>Welcome to Grandma's Pantry, where every recipe is more than just a list of ingredients—it's a story waiting to be told. Inspired by the warmth and tradition of family kitchens, our mission is to empower cooks of all levels to create meals that bring people together.</p>
-          <p>Whether you're exploring cherished classics or experimenting with new flavors, Grandma's Pantry is here to guide you every step of the way. Dive into our collection, share the joy of cooking, and start crafting memories one recipe at a time. Your culinary adventure begins here!</p>
-        </article>
-        <img src="assets/pancake-skillet.png" width="478" height="615" alt="A black skillet with pancakes and mixed berries.">
+        <section>
+          <article>
+            <h2>The Heart of Grandma's Pantry</h2>
+            <p>Welcome to Grandma's Pantry, where every recipe is more than just a list of ingredients—it's a story waiting to be told. Inspired by the warmth and tradition of family kitchens, our mission is to empower cooks of all levels to create meals that bring people together.</p>
+            <p>Whether you're exploring cherished classics or experimenting with new flavors, Grandma's Pantry is here to guide you every step of the way. Dive into our collection, share the joy of cooking, and start crafting memories one recipe at a time. Your culinary adventure begins here!</p>
+          </article>
+          <img src="assets/pancake-skillet.png" width="478" height="615" alt="A black skillet with pancakes and mixed berries.">
+        </section>
       </main>
     </div>
     <footer>
